41098fe09b6a9964e4c477d41f455f45ab2077de
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-09-17 13:46:14 +0100

    transformed to a more Ruby style

--- twitt.rb
+++ twitt.rb
@@ -1,24 +1,20 @@
 require 'twitter'
 require './configure'
 
-screen_name = String.new ARGV[0]
+screen_name = ARGV[0] || 'timoreilly'
 a_user = Twitter.user(screen_name)
 
-if a_user.protected != true
-  puts "Username   : " + a_user.screen_name.to_s
-  puts "Name       : " + a_user.name
-  puts "Location   : " + a_user.location
-  if (a_user.url != nil)
-    puts "URL        : " + a_user.url.to_s
-  end
-  puts "Verified   : " + a_user.verified.to_s
-  puts
+puts "Username   : #{a_user.screen_name}"
+puts "Name       : #{a_user.name}"
+puts "Location   : #{a_user.location}"
+puts "URL        : #{a_user.url}" if (a_user.url != nil)
+puts "Verified   : #{a_user.verified}"
+puts
 
-  tweet = Twitter.user_timeline(screen_name).first
+tweet = Twitter.user_timeline(screen_name).first
 
-  if tweet
-    puts "Tweet text : " + tweet.text 
-    puts "Tweet time : " + tweet.created_at.to_s
-    puts "Tweet ID   : " + tweet.id.to_s
-  end
+if tweet
+  puts "Tweet text : #{tweet.text }"
+  puts "Tweet time : #{tweet.created_at}"
+  puts "Tweet ID   : #{tweet.id}"
 end
